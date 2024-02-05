70a4e824394189c1d3fb381621fc3020354a78ad
Abhilash <aramadugu@idt.net>
2024-02-05 16:11:02 +0530

    added the config.h

---
+++ include/eixx/config.h
@@ -0,0 +1,163 @@
+/* Define to 1 if you have the `gettimeofday' function. */
+#ifndef EIXX_HAVE_GETTIMEOFDAY
+#define EIXX_HAVE_GETTIMEOFDAY 1
+#endif
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#ifndef EIXX_HAVE_INTTYPES_H
+#define EIXX_HAVE_INTTYPES_H 1
+#endif
+
+/* Define to 1 if you have the <memory.h> header file. */
+#ifndef EIXX_HAVE_MEMORY_H
+#define EIXX_HAVE_MEMORY_H 1
+#endif
+
+/* Define to 1 if you have the <openssl/md5.h> header file. */
+#ifndef EIXX_HAVE_OPENSSL_MD5_H
+#define EIXX_HAVE_OPENSSL_MD5_H 1
+#endif
+
+/* Define to 1 if you have the <openssl/opensslconf.h> header file. */
+#ifndef EIXX_HAVE_OPENSSL_OPENSSLCONF_H
+#define EIXX_HAVE_OPENSSL_OPENSSLCONF_H 1
+#endif
+
+/* Define to 1 if you have the `socket' function. */
+#ifndef EIXX_HAVE_SOCKET
+#define EIXX_HAVE_SOCKET 1
+#endif
+
+/* Define to 1 if you have the `sqrt' function. */
+/* #undef HAVE_SQRT */
+
+/* Define to 1 if stdbool.h conforms to C99. */
+#ifndef EIXX_HAVE_STDBOOL_H
+#define EIXX_HAVE_STDBOOL_H 1
+#endif
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#ifndef EIXX_HAVE_STDINT_H
+#define EIXX_HAVE_STDINT_H 1
+#endif
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#ifndef EIXX_HAVE_STDLIB_H
+#define EIXX_HAVE_STDLIB_H 1
+#endif
+
+/* Define to 1 if you have the <strings.h> header file. */
+#ifndef EIXX_HAVE_STRINGS_H
+#define EIXX_HAVE_STRINGS_H 1
+#endif
+
+/* Define to 1 if you have the <string.h> header file. */
+#ifndef EIXX_HAVE_STRING_H
+#define EIXX_HAVE_STRING_H 1
+#endif
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#ifndef EIXX_HAVE_SYS_STAT_H
+#define EIXX_HAVE_SYS_STAT_H 1
+#endif
+
+/* Define to 1 if you have the <sys/time.h> header file. */
+#ifndef EIXX_HAVE_SYS_TIME_H
+#define EIXX_HAVE_SYS_TIME_H 1
+#endif
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#ifndef EIXX_HAVE_SYS_TYPES_H
+#define EIXX_HAVE_SYS_TYPES_H 1
+#endif
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#ifndef EIXX_HAVE_UNISTD_H
+#define EIXX_HAVE_UNISTD_H 1
+#endif
+
+/* Define to 1 if the system has the type `_Bool'. */
+#ifndef EIXX_HAVE__BOOL
+#define EIXX_HAVE__BOOL 1
+#endif
+
+/* Define to the sub-directory in which libtool stores uninstalled libraries.
+   */
+#ifndef EIXX_LT_OBJDIR
+#define EIXX_LT_OBJDIR ".libs/"
+#endif
+
+/* Name of package */
+#ifndef EIXX_PACKAGE
+#define EIXX_PACKAGE "eixx"
+#endif
+
+/* Define to the address where bug reports for this package should be sent. */
+#ifndef EIXX_PACKAGE_BUGREPORT
+#define EIXX_PACKAGE_BUGREPORT "BUG-REPORT-ADDRESS"
+#endif
+
+/* Define to the full name of this package. */
+#ifndef EIXX_PACKAGE_NAME
+#define EIXX_PACKAGE_NAME "eixx"
+#endif
+
+/* Define to the full name and version of this package. */
+#ifndef EIXX_PACKAGE_STRING
+#define EIXX_PACKAGE_STRING "eixx 0.1"
+#endif
+
+/* Define to the one symbol short name of this package. */
+#ifndef EIXX_PACKAGE_TARNAME
+#define EIXX_PACKAGE_TARNAME "eixx"
+#endif
+
+/* Define to the home page for this package. */
+#ifndef EIXX_PACKAGE_URL
+#define EIXX_PACKAGE_URL ""
+#endif
+
+/* Define to the version of this package. */
+#ifndef EIXX_PACKAGE_VERSION
+#define EIXX_PACKAGE_VERSION "0.1"
+#endif
+
+/* Define to 1 if you have the ANSI C header files. */
+#ifndef EIXX_STDC_HEADERS
+#define EIXX_STDC_HEADERS 1
+#endif
+
+/* Version number of package */
+#ifndef EIXX_VERSION
+#define EIXX_VERSION "0.1"
+#endif
+
+/* Define for Solaris 2.5.1 so the uint32_t typedef from <sys/synch.h>,
+   <pthread.h>, or <semaphore.h> is not used. If the typedef were allowed, the
+   #define below would cause a syntax error. */
+/* #undef _UINT32_T */
+
+/* Define for Solaris 2.5.1 so the uint64_t typedef from <sys/synch.h>,
+   <pthread.h>, or <semaphore.h> is not used. If the typedef were allowed, the
+   #define below would cause a syntax error. */
+/* #undef _UINT64_T */
+
+/* Define to `__inline__' or `__inline' if that's what the C compiler
+   calls it, or to nothing if 'inline' is not supported under any name.  */
+#ifndef __cplusplus
+/* #undef inline */
+#endif
+
+/* Define to `unsigned int' if <sys/types.h> does not define. */
+/* #undef size_t */
+
+/* Define to the type of an unsigned integer type of width exactly 32 bits if
+   such a type exists and the standard includes do not define it. */
+/* #undef uint32_t */
+
+/* Define to the type of an unsigned integer type of width exactly 64 bits if
+   such a type exists and the standard includes do not define it. */
+/* #undef uint64_t */
+ 
+/* once: _INCLUDE_EIXX_CONFIG_H */
+#endif
