d16480601e8a1117e4e0234e402aac3e66621dc2
Alex Pol <apolonevich@idt.net>
2024-05-21 12:52:52 +0300

    moving to the latest eixx version

--- include/eixx/eterm.hpp
+++ include/eixx/eterm.hpp
@@ -28,7 +28,6 @@ limitations under the License.
 #ifndef _EIXX_ETERM_HPP_
 #define _EIXX_ETERM_HPP_
 
-#include <eixx/config.h>
 #include <eixx/marshal/defaults.hpp>
 #include <eixx/marshal/eterm.hpp>
 
@@ -38,24 +37,24 @@ limitations under the License.
 
 namespace eixx {
 
-typedef marshal::eterm<allocator_t>                  eterm;
-typedef marshal::atom                                atom;
-typedef marshal::string<allocator_t>                 string;
-typedef marshal::binary<allocator_t>                 binary;
-typedef marshal::epid<allocator_t>                   epid;
-typedef marshal::port<allocator_t>                   port;
-typedef marshal::ref<allocator_t>                    ref;
-typedef marshal::tuple<allocator_t>                  tuple;
-typedef marshal::list<allocator_t>                   list;
-typedef marshal::map<allocator_t>                    map;
-typedef marshal::trace<allocator_t>                  trace;
-typedef marshal::var                                 var;
-typedef marshal::varbind<allocator_t>                varbind;
-typedef marshal::eterm_pattern_matcher<allocator_t>  eterm_pattern_matcher;
-typedef marshal::eterm_pattern_action<allocator_t>   eterm_pattern_action;
+typedef marshal::eterm<EIXX_NAMESPACE::allocator_t>                  eterm;
+typedef marshal::atom                                                atom;
+typedef marshal::string<EIXX_NAMESPACE::allocator_t>                 string;
+typedef marshal::binary<EIXX_NAMESPACE::allocator_t>                 binary;
+typedef marshal::epid<EIXX_NAMESPACE::allocator_t>                   epid;
+typedef marshal::port<EIXX_NAMESPACE::allocator_t>                   port;
+typedef marshal::ref<EIXX_NAMESPACE::allocator_t>                    ref;
+typedef marshal::tuple<EIXX_NAMESPACE::allocator_t>                  tuple;
+typedef marshal::list<EIXX_NAMESPACE::allocator_t>                   list;
+typedef marshal::map<EIXX_NAMESPACE::allocator_t>                    map;
+typedef marshal::trace<EIXX_NAMESPACE::allocator_t>                  trace;
+typedef marshal::var                                                 var;
+typedef marshal::varbind<EIXX_NAMESPACE::allocator_t>                varbind;
+typedef marshal::eterm_pattern_matcher<EIXX_NAMESPACE::allocator_t>  eterm_pattern_matcher;
+typedef marshal::eterm_pattern_action<EIXX_NAMESPACE::allocator_t>   eterm_pattern_action;
 
 namespace detail {
-    BOOST_STATIC_ASSERT(sizeof(eterm)     == (ALIGNOF_UINT64_T > sizeof(int) ? ALIGNOF_UINT64_T : sizeof(int)) + sizeof(uint64_t));
+    BOOST_STATIC_ASSERT(sizeof(eterm)     == (alignof(uint64_t) > sizeof(int) ? alignof(uint64_t) : sizeof(int)) + sizeof(uint64_t));
     BOOST_STATIC_ASSERT(sizeof(atom)      <= sizeof(uint64_t));
     BOOST_STATIC_ASSERT(sizeof(string)    <= sizeof(uint64_t));
     BOOST_STATIC_ASSERT(sizeof(binary)    <= sizeof(uint64_t));
